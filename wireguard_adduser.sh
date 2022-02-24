6f08f0d7eaa78cae1dcebf59383765748d211b55
david.klein@msb.nrw.de <david.klein@msb.nrw.de>
2022-02-24 17:18:25 +0100

    deleted:    wireguard_add_user.sh 	new file:   wireguard_adduser.sh 	new file:   wireguard_deluser.sh

---
+++ wireguard_adduser.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+# based on 
+# https://github.com/Nyr/wireguard-install
+#
+# Copyright (c) 2020 Nyr. Released under the MIT License.
+# Copyright (c) Changes 2022 David Klein. Released under the MIT License.
+
+
+# Detect Debian users running the script with "sh" instead of bash
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo 'This installer needs to be run with "bash", not "sh".'
+	exit
+fi
+
+# Detect environments where $PATH does not include the sbin directories
+if ! grep -q sbin <<< "$PATH"; then
+	echo '$PATH does not include sbin. Try using "su -" instead of "su".'
+	exit
+fi
+
+# Are you root?
+if [[ "$EUID" -ne 0 ]]; then
+	echo "This installer needs to be run with superuser privileges."
+	exit
+fi
+
+# is there a wirguard config? 
+if [[ ! -e /etc/wireguard/wg0.conf ]]; then
+	clear
+	echo 'Please install WireGuard with road warrior installer first!'
+fi
+
+# can i find dependies? 
+#if [[ ! -e /etc/wireguard/wg0.conf ]]; then
+#	clear
+#	echo 'Please install WireGuard with road warrior installer first!'
+#fi
+
+
+new_client_setup () {
+	# Given a list of the assigned internal IPv4 addresses, obtain the lowest still
+	# available octet. Important to start looking at 2, because 1 is our gateway.
+	octet=2
+	while grep AllowedIPs /etc/wireguard/wg0.conf | cut -d "." -f 4 | cut -d "/" -f 1 | grep -q "$octet"; do
+		(( octet++ ))
+	done
+	# Don't break the WireGuard configuration in case the address space is full
+	if [[ "$octet" -eq 255 ]]; then
+		echo "253 clients are already configured. The WireGuard internal subnet is full!"
+		exit
+	fi
+	key=$(wg genkey)
+	psk=$(wg genpsk)
+	# Configure client in the server
+	cat << EOF >> /etc/wireguard/wg0.conf
+# BEGIN_PEER $client
+[Peer]
+PublicKey = $(wg pubkey <<< $key)
+PresharedKey = $psk
+AllowedIPs = 10.7.0.$octet/32$(grep -q 'fddd:2c4:2c4:2c4::1' /etc/wireguard/wg0.conf && echo ", fddd:2c4:2c4:2c4::$octet/128")
+# END_PEER $client
+EOF
+	# Create client configuration
+	cat << EOF > ~/"$client".conf
+[Interface]
+Address = 10.7.0.$octet/24$(grep -q 'fddd:2c4:2c4:2c4::1' /etc/wireguard/wg0.conf && echo ", fddd:2c4:2c4:2c4::$octet/64")
+DNS = $dns
+PrivateKey = $key
+
+[Peer]
+PublicKey = $(grep PrivateKey /etc/wireguard/wg0.conf | cut -d " " -f 3 | wg pubkey)
+PresharedKey = $psk
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = $(grep '^# ENDPOINT' /etc/wireguard/wg0.conf | cut -d " " -f 3):$(grep ListenPort /etc/wireguard/wg0.conf | cut -d " " -f 3)
+PersistentKeepalive = 25
+EOF
+}
+
+
+new_client_dns () {
+
+
+dns = 1
+	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
+		echo "$dns: invalid selection."
+		read -p "DNS server [1]: " dns
+	done
+		# DNS
+	case "$dns" in
+		1|"")
+			# Locate the proper resolv.conf
+			# Needed for systems running systemd-resolved
+			if grep -q '^nameserver 127.0.0.53' "/etc/resolv.conf"; then
+				resolv_conf="/run/systemd/resolve/resolv.conf"
+			else
+				resolv_conf="/etc/resolv.conf"
+			fi
+			# Extract nameservers and provide them in the required format
+			dns=$(grep -v '^#\|^;' "$resolv_conf" | grep '^nameserver' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | xargs | sed -e 's/ /, /g')
+		;;
+		2)
+			dns="8.8.8.8, 8.8.4.4"
+		;;
+		3)
+			dns="1.1.1.1, 1.0.0.1"
+		;;
+		4)
+			dns="208.67.222.222, 208.67.220.220"
+		;;
+		5)
+			dns="9.9.9.9, 149.112.112.112"
+		;;
+		6)
+			dns="94.140.14.14, 94.140.15.15"
+		;;
+	esac
+}
+
+
+
+# ToDo: Name abholen
+	
+	# new user: 
+	unsanitized_client = "test"
+	
+	# Allow a limited set of characters to avoid conflicts
+	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
+	echo
+	new_client_dns
+	new_client_setup
+	# Append new client configuration to the WireGuard interface
+	wg addconf wg0 <(sed -n "/^# BEGIN_PEER $client/,/^# END_PEER $client/p" /etc/wireguard/wg0.conf)
+	echo
+	qrencode -t UTF8 < ~/"$client.conf"
+	echo -e '\xE2\x86\x91 That is a QR code containing your client configuration.'
+	echo
+	echo "$client added. Configuration available in:" ~/"$client.conf"
+#exit
+	
+
+
---
+++ wireguard_deluser.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+# based on 
+# https://github.com/Nyr/wireguard-install
+#
+# Copyright (c) 2020 Nyr. Released under the MIT License.
+# Copyright (c) Changes 2022 David Klein. Released under the MIT License.
+
+
+# Detect Debian users running the script with "sh" instead of bash
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo 'This installer needs to be run with "bash", not "sh".'
+	exit
+fi
+
+# Detect environments where $PATH does not include the sbin directories
+if ! grep -q sbin <<< "$PATH"; then
+	echo '$PATH does not include sbin. Try using "su -" instead of "su".'
+	exit
+fi
+
+# Are you root?
+if [[ "$EUID" -ne 0 ]]; then
+	echo "This installer needs to be run with superuser privileges."
+	exit
+fi
+
+# is there a wirguard config? 
+if [[ ! -e /etc/wireguard/wg0.conf ]]; then
+	clear
+	echo 'Please install WireGuard with road warrior installer first!'
+fi
+
+# can i find nessesery Environment? 
+#if [[ ! -e /etc/wireguard/wg0.conf ]]; then
+#	clear
+#	echo 'Please install WireGuard with road warrior installer first!'
+#fi
+
+
+new_client_setup () {
+	# Given a list of the assigned internal IPv4 addresses, obtain the lowest still
+	# available octet. Important to start looking at 2, because 1 is our gateway.
+	octet=2
+	while grep AllowedIPs /etc/wireguard/wg0.conf | cut -d "." -f 4 | cut -d "/" -f 1 | grep -q "$octet"; do
+		(( octet++ ))
+	done
+	# Don't break the WireGuard configuration in case the address space is full
+	if [[ "$octet" -eq 255 ]]; then
+		echo "253 clients are already configured. The WireGuard internal subnet is full!"
+		exit
+	fi
+	key=$(wg genkey)
+	psk=$(wg genpsk)
+	# Configure client in the server
+	cat << EOF >> /etc/wireguard/wg0.conf
+# BEGIN_PEER $client
+[Peer]
+PublicKey = $(wg pubkey <<< $key)
+PresharedKey = $psk
+AllowedIPs = 10.7.0.$octet/32$(grep -q 'fddd:2c4:2c4:2c4::1' /etc/wireguard/wg0.conf && echo ", fddd:2c4:2c4:2c4::$octet/128")
+# END_PEER $client
+EOF
+	# Create client configuration
+	cat << EOF > ~/"$client".conf
+[Interface]
+Address = 10.7.0.$octet/24$(grep -q 'fddd:2c4:2c4:2c4::1' /etc/wireguard/wg0.conf && echo ", fddd:2c4:2c4:2c4::$octet/64")
+DNS = $dns
+PrivateKey = $key
+
+[Peer]
+PublicKey = $(grep PrivateKey /etc/wireguard/wg0.conf | cut -d " " -f 3 | wg pubkey)
+PresharedKey = $psk
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = $(grep '^# ENDPOINT' /etc/wireguard/wg0.conf | cut -d " " -f 3):$(grep ListenPort /etc/wireguard/wg0.conf | cut -d " " -f 3)
+PersistentKeepalive = 25
+EOF
+}
+
+
+new_client_dns () {
+#	echo "Select a DNS server for the client:"
+#	echo "   1) Current system resolvers"
+#	echo "   2) Google"
+#	echo "   3) 1.1.1.1"
+#	echo "   4) OpenDNS"
+#	echo "   5) Quad9"
+#	echo "   6) AdGuard"
+#	read -p "DNS server [1]: " dns
+
+dns = 1
+	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
+		echo "$dns: invalid selection."
+		read -p "DNS server [1]: " dns
+	done
+		# DNS
+	case "$dns" in
+		1|"")
+			# Locate the proper resolv.conf
+			# Needed for systems running systemd-resolved
+			if grep -q '^nameserver 127.0.0.53' "/etc/resolv.conf"; then
+				resolv_conf="/run/systemd/resolve/resolv.conf"
+			else
+				resolv_conf="/etc/resolv.conf"
+			fi
+			# Extract nameservers and provide them in the required format
+			dns=$(grep -v '^#\|^;' "$resolv_conf" | grep '^nameserver' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | xargs | sed -e 's/ /, /g')
+		;;
+		2)
+			dns="8.8.8.8, 8.8.4.4"
+		;;
+		3)
+			dns="1.1.1.1, 1.0.0.1"
+		;;
+		4)
+			dns="208.67.222.222, 208.67.220.220"
+		;;
+		5)
+			dns="9.9.9.9, 149.112.112.112"
+		;;
+		6)
+			dns="94.140.14.14, 94.140.15.15"
+		;;
+	esac
+}
+
+
+
+# Name abholen:
+	
+	# Nyr way:  
+	#echo "Enter a name for the first client:"
+	#read -p "Name [client]: " unsanitized_client
+	
+	# new user: 
+	unsanitized_client = "test"
+	
+	# Allow a limited set of characters to avoid conflicts
+	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
+	[[ -z "$client" ]] && client="client"
+	echo
+	new_client_dns
+	echo "New WireGuard user $unsanitized_client will be installed."
+	# read -n1 -r -p "Press any key to continue..."
+	# Generates the custom client.conf
+	new_client_setup
+	# Enable and start the wg-quick service
+	systemctl enable --now wg-quick@wg0.service
+	echo
+	qrencode -t UTF8 < ~/"$client.conf"
+	echo -e '\xE2\x86\x91 That is a QR code containing the client configuration.'
+	echo
+
+	# [ab hier wirds eklig]
+
+#	echo "   1) Add a new client"
+#	echo "   2) Remove an existing client"
+
+	case "$option" in
+		1)
+			echo
+			echo "Provide a name for the client:"
+			read -p "Name: " unsanitized_client
+			# Allow a limited set of characters to avoid conflicts
+			client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
+			while [[ -z "$client" ]] || grep -q "^# BEGIN_PEER $client$" /etc/wireguard/wg0.conf; do
+				echo "$client: invalid name."
+				read -p "Name: " unsanitized_client
+				client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
+			done
+			echo
+			new_client_dns
+			new_client_setup
+			# Append new client configuration to the WireGuard interface
+			wg addconf wg0 <(sed -n "/^# BEGIN_PEER $client/,/^# END_PEER $client/p" /etc/wireguard/wg0.conf)
+			echo
+			qrencode -t UTF8 < ~/"$client.conf"
+			echo -e '\xE2\x86\x91 That is a QR code containing your client configuration.'
+			echo
+			echo "$client added. Configuration available in:" ~/"$client.conf"
+			exit
+		;;
+		2)
+			# This option could be documented a bit better and maybe even be simplified
+			# ...but what can I say, I want some sleep too
+			number_of_clients=$(grep -c '^# BEGIN_PEER' /etc/wireguard/wg0.conf)
+			if [[ "$number_of_clients" = 0 ]]; then
+				echo
+				echo "There are no existing clients!"
+				exit
+			fi
+			echo
+			echo "Select the client to remove:"
+			grep '^# BEGIN_PEER' /etc/wireguard/wg0.conf | cut -d ' ' -f 3 | nl -s ') '
+			read -p "Client: " client_number
+			until [[ "$client_number" =~ ^[0-9]+$ && "$client_number" -le "$number_of_clients" ]]; do
+				echo "$client_number: invalid selection."
+				read -p "Client: " client_number
+			done
+			client=$(grep '^# BEGIN_PEER' /etc/wireguard/wg0.conf | cut -d ' ' -f 3 | sed -n "$client_number"p)
+			echo
+			read -p "Confirm $client removal? [y/N]: " remove
+			until [[ "$remove" =~ ^[yYnN]*$ ]]; do
+				echo "$remove: invalid selection."
+				read -p "Confirm $client removal? [y/N]: " remove
+			done
+			if [[ "$remove" =~ ^[yY]$ ]]; then
+				# The following is the right way to avoid disrupting other active connections:
+				# Remove from the live interface
+				wg set wg0 peer "$(sed -n "/^# BEGIN_PEER $client$/,\$p" /etc/wireguard/wg0.conf | grep -m 1 PublicKey | cut -d " " -f 3)" remove
+				# Remove from the configuration file
+				sed -i "/^# BEGIN_PEER $client/,/^# END_PEER $client/d" /etc/wireguard/wg0.conf
+				echo
+				echo "$client removed!"
+			else
+				echo
+				echo "$client removal aborted!"
+			fi
+			exit
+		;;
+
+	esac
+fi
